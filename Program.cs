2a5bbdf799a1f128537f2ba8d188011d12f94fee
LinksNet72 <gakolade@gmail.com>
2019-06-01 12:47:13 +0100

    My first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AI6GitBash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
